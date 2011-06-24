e1889a6827b0b81a3717d46f7a8a321ad7a608eb
Jason Jarrett <jason@elegantcode.com>
2011-06-24 12:43:41 -0700

    Moved a class into it's own file

---
+++ FileByter/TypeConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace FileByter
+{
+	public class TypeConfiguration
+	{
+		private readonly PropertiesCollection _properties = new PropertiesCollection();
+
+		public PropertiesCollection Properties { get { return _properties; } }
+
+		public void AddProperty(Property property)
+		{
+			var propertyName = property.PropertyName;
+
+			// Should only add property once
+			if (Properties.ContainsPropertyName(propertyName))
+				throw new ArgumentException("The property [{0}] has been already been specified.".FormatWith(propertyName));
+
+			Properties.AddProperty(propertyName, property);
+		}
+	}
+
+
+	public class TypeConfiguration<T> : TypeConfiguration
+	{
+		public TypeConfiguration<T> Exclude<TProperty>(Expression<Func<T, TProperty>> propertyExpression)
+		{
+			if (propertyExpression == null) throw new ArgumentNullException("propertyExpression");
+			var propertyName = propertyExpression.GetMemberName();
+			Properties.AddExclusion(propertyName);
+			return this;
+		}
+
+
+		public TypeConfiguration<T> AddPropertyFormatter<TProperty>(Expression<Func<T, TProperty>> propertyExpression, PropertyFormatter propertyFormatter, HeaderFormatter headerFormatter)
+		{
+			if (propertyExpression == null) throw new ArgumentNullException("propertyExpression");
+			if (propertyFormatter == null) throw new ArgumentNullException("propertyFormatter");
+
+			var propertyName = propertyExpression.GetMemberName();
+
+			if (headerFormatter == null)
+				headerFormatter = pi => pi.Name;
+
+			var property = new Property(typeof(T), propertyName, propertyFormatter, headerFormatter);
+
+			Properties.AddProperty(propertyName, property);
+
+			return this;
+		}
+		public TypeConfiguration<T> AddPropertyFormatter<TProperty>(Expression<Func<T, TProperty>> propertyExpression, PropertyFormatter formatter)
+		{
+			return AddPropertyFormatter(propertyExpression, formatter, null);
+		}
+	}
+}
